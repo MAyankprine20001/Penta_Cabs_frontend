10a7f0d5261853eb4bd360c2867cfe82bc71fa66
MayankPandey19136016 <mayankpandeyji2001@gmail.com>
2025-12-11 23:09:15 +0530

    change the env

--- src/config/environment.ts
+++ src/config/environment.ts
@@ -4,7 +4,7 @@
 export const environment = {
   // Base URLs
   // baseUrl: 'https://k4n3x29l-5000.inc1.devtunnels.ms/'
-  // baseUrl:'https://pentacabbackend-production.up.railway.app'
-  baseUrl:"https://penta-cab-backend.onrender.com"
+  baseUrl:'https://pentacabbackend-production.up.railway.app'
+  // baseUrl:"https://penta-cab-backend.onrender.com"
   // baseUrl: 'http://localhost:5000'
 } 
